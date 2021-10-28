5f747607f2639cf7d08df35add6237fd0b50e3ea
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-10-28 10:10:41 -0500

    modify: Finishing the Arrays assingment

--- src/assingments/Arrays.java
+++ src/assingments/Arrays.java
@@ -2,6 +2,18 @@ package assingments;
 
 public class Arrays {
   public static void main(String[] args) {
-    
+    String[] names = {
+      "Gerardo",
+      "Adalberto",
+      "Josué",
+      "Roberto",
+      "Miranda",
+      "Nahomi",
+      "Ximena"
+    };
+
+    for (int i = names.length - 1; i >= 0; i--) {
+      System.out.println(names[i]);
+    }
   }
 }
