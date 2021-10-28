d0850305f325f03282e52a6d47f17e8b89d49959
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-10-27 22:13:54 -0500

    modify: Finishing MyImmutable class

--- src/immutable/MyImmutable.java
+++ src/immutable/MyImmutable.java
@@ -1,5 +1,26 @@
 package immutable;
 
 public class MyImmutable {
-  
+  private int x;
+
+  public MyImmutable(int x) {
+    this.x = x;
+  }
+
+  public MyImmutable change(int x) {
+    if (this.x == x) {
+      return this;
+    }
+
+    return new MyImmutable(x);
+  }
+
+  public static void main(String[] args) {
+    MyImmutable referenceOne = new MyImmutable(20);
+    MyImmutable referenceTwo = referenceOne.change(20);
+    System.out.println(referenceOne == referenceTwo);
+
+    MyImmutable referenceThree = referenceTwo.change(50);
+    System.out.println(referenceThree == referenceTwo);
+  }
 }
