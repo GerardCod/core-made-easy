4194188411f0f7c3a0c599763a4ed3e906f1123f
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-10-28 09:46:54 -0500

    modify: Finishing FiveWithZero class

--- src/arrays/FiveWithZero.java
+++ src/arrays/FiveWithZero.java
@@ -2,6 +2,16 @@ package arrays;
 
 public class FiveWithZero {
   public static void main(String[] args) {
+    int a[] = {3, 4, 6, 3, 5, 7, 3};
     
+    for (int i = 0; i < a.length - 1; i++) {
+      if (a[i] == 3 && a[i + 1] == 5) {
+        a[i + 1] = 0;
+      }
+    }
+
+    for (int i : a) {
+      System.out.println(i);
+    }
   }
 }
