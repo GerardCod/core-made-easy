887a98fc489c9de5b0e6a631da58c973cbcd7004
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-10-28 09:34:16 -0500

    modify: Finishing ArraysDemo class

--- src/arrays/ArraysDemo.java
+++ src/arrays/ArraysDemo.java
@@ -2,6 +2,34 @@ package arrays;
 
 public class ArraysDemo {
   public static void main(String[] args) {
+    // Formas de declarar un arreglo.
     
+    //int arr[5];
+    
+    //int arr[] = {10, 20, 30, 40, 50};
+
+    int arr[] = new int[5];
+
+    arr[0] = 10;
+    arr[1] = Integer.valueOf(20); // Autoboxing
+    arr[2] = 30;
+    arr[3] = 40;
+    arr[4] = 50;
+
+    System.out.println("Elements of the array");
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(i);
+    }
+    
+    System.out.println("Elements of the array");
+    for (int i : arr) {
+      System.out.println(i);
+    }
+
+
+    long[] arr1 = new long[4];
+    for (long l : arr1) {
+      System.out.println(l);
+    }
   }
 }
