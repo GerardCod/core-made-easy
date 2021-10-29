48f755f1603bb8573a3efe05fb88a7f30450a9a8
GerardCod <gerardo.aguilar@alumno.uttehuacah.edu.mx>
2021-10-28 20:15:13 -0500

    modify: Trying to do the practice of Class loading

---
+++ src/jvm/User.java
@@ -0,0 +1,13 @@
+package jvm;
+
+public class User {
+
+    public String getName() {
+        return "Gerardo";
+    }
+
+    public String getAddress() {
+        return "Calle Siempre Viva 2434";
+    }
+
+}
